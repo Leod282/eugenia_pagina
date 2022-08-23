ab3750039e2e9d76e81f3b8c56321fd7a7faf834
LeoD <103426903+Leod282@users.noreply.github.com>
2022-08-23 19:27:36 -0300

    Add files via upload

---
+++ euge_pagina/js/js.js
@@ -0,0 +1,15 @@
+const iconoMenu = document.querySelector("#icono-menu"),
+    menu = document.querySelector('#menu');
+
+iconoMenu.addEventListener('click', (e) =>{
+    menu.classList.toggle('active');
+    document.body.classList.toggle('opacity');
+
+  /*  const rutaActual = e.target.getAttribute('src');
+    if(rutaActual == 'imagenes/menu1.png'){
+        e.target.setAttribute('src', 'imagenes/menu.png');
+
+    }else{
+        e.target.setAttribute('src', 'imagenes/menu1.png')
+    }*/
+} );
